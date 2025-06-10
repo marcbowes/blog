6bf742c7b0c74788321d8ceb63fcb76bb370693e
Marc Bowes <15209+marcbowes@users.noreply.github.com>
2025-06-10 15:25:25 -0700

    rss

--- publish.el
+++ publish.el
@@ -1,5 +1,47 @@
 ;;; publish.el -*- lexical-binding: t; -*-
 
+;; https://writepermission.com/org-blogging-rss-feed.html
+
+(defun +blog/org-rss-publish-to-rss (plist filename pub-dir)
+  "Publish RSS with PLIST, only when FILENAME is 'rss.org'.
+PUB-DIR is when the output will be placed."
+  (if (equal "rss.org" (file-name-nondirectory filename))
+      (org-rss-publish-to-rss plist filename pub-dir)))
+
+(defun +blog/format-rss-feed (title list)
+  "Generate RSS feed, as a string.
+TITLE is the title of the RSS feed.  LIST is an internal
+representation for the files to include, as returned by
+`org-list-to-lisp'.  PROJECT is the current project."
+  (concat "#+TITLE: " title "\n\n"
+          (org-list-to-subtree list 1 '(:icount "" :istart ""))))
+
+(defun +blog/format-rss-feed-entry (entry style project)
+  "Format ENTRY for the RSS feed.
+ENTRY is a file name.  STYLE is either 'list' or 'tree'.
+PROJECT is the current project."
+  (cond ((not (directory-name-p entry))
+         (let* ((file (org-publish--expand-file-name entry project))
+                (title (org-publish-find-title entry project))
+                (date (format-time-string "%Y-%m-%d" (org-publish-find-date entry project)))
+                (link (concat (file-name-sans-extension entry) ".html")))
+           (with-temp-buffer
+             (org-mode)
+             (insert (format "* [[file:%s][%s]]\n" file title))
+             (org-set-property "RSS_PERMALINK" link)
+             (org-set-property "PUBDATE" date)
+             (insert-file-contents file)
+             (buffer-string)
+             (goto-char (point-min))
+             (forward-paragraph 3)
+             (delete-region (point) (point-max))
+             (buffer-string))
+           ))
+        ((eq style 'tree)
+         ;; Return only last subdir.
+         (file-name-nondirectory (directory-file-name entry)))
+        (t entry)))
+
 (setq org-publish-project-alist
       `(("blog"
          :base-directory "~/code/blog/src/org/content/"
@@ -9,10 +51,23 @@
          :with-toc nil
          :html-head-include-default-style nil
          :html-head-include-scripts nil
-         :html-head-extra ,(with-temp-buffer
-                             (insert-file-contents "~/code/blog/src/org/templates/head.html")
-                             (buffer-string))
          :html-preamble nil)
+        ("blog-rss"
+         :base-directory "~/code/blog/src/org/content/"
+         :exclude ,(regexp-opt '("rss.org" "index.org" "404.org"))
+         :publishing-function +blog/org-rss-publish-to-rss
+         :publishing-directory "~/code/blog/generated/"
+         :rss-extension "xml"
+         :html-link-home "https://marc-bowes.com"
+         :html-link-use-abs-url t
+         :html-link-org-files-as-html t
+         :auto-sitemap t
+         :sitemap-filename "rss.org"
+         :sitemap-title "marc-bowes.com"
+         :sitemap-style list
+         :sitemap-sort-files anti-chronologically
+         :sitemap-function +blog/format-rss-feed
+         :sitemap-format-entry +blog/format-rss-feed-entry)
         ("org-created"
          :base-directory "~/code/blog/src/org/content/images"
          :base-extension "jpg\\|gif\\|png"
